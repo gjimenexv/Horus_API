2a43df3fd84492fb20e1f875ba18638fa957be26
Guillermo Jimenez <guillejim5@gmail.com>
2020-11-04 21:21:10 -0600

    Creacion de la clase clsBitacora para trazabilidad de cambios en la BD + clsLogin que invoca el sp

---
+++ Solution.BS/clsLogin.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Solution.DAL;
+
+namespace Solution.BS
+{
+    public class clsLogin
+    {
+        HorusDataContext dc = new HorusDataContext();
+
+        public List<AutenticarUsuarioResult> AutenticarUsuario(string usuario, string contraseña)
+        {
+            try
+            {
+                List<AutenticarUsuarioResult> data = dc.AutenticarUsuario(usuario, contraseña).ToList();
+                return data;
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+        }
+    }
+}
