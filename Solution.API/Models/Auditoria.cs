ab355ee49f4c54d39912b19696682930c5b22d87
LorianAg <loriana.aguilar02@gmail.com>
2020-11-21 15:53:20 -0600

    Creacion del modelo de la tabla Auditoria

---
+++ Solution.API/Models/Auditoria.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Solution.API.Models
+{
+    public class Auditoria
+    {
+        public int IdAuditoria { get; set; }
+        public string Tabla { get; set; }
+        public string Registro { get; set; }
+        public string Accion { get; set; }
+        public DateTime Fecha { get; set; }
+        public string IdUsuario { get; set; }
+    }
+}
